3e703f166acd8893c6510f826e370b353542e494
kevinortiz43 <93737536+kevinortiz43@users.noreply.github.com>
2022-02-06 19:31:59 -0500

    Homework Submission

---
+++ UserInputHW3.java
@@ -0,0 +1,109 @@
+import java.util.Scanner;
+
+class UserInputHW3{
+    public static void main(String args[]){
+
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Enter a number  to be armstronged");
+        String UserInput = scan.nextLine(); 
+        Boolean Armstrong = false;
+        int Digits = UserInput.length();
+            System.out.println(Digits + " number of digits ");
+            System.out.println(UserInput + " is the user input ");
+
+        for(int i=0; i<Digits; i++){
+        // System.out.println(UserInput.charAt(i));
+        // this prints out the numbers seperaterly
+        
+            int number = Character.getNumericValue(UserInput.charAt(i));
+            // System.out.println(number);
+
+            int Check =  (int) Math.pow(number,Digits);
+            
+            // String ToBeChecked = (String) Check.AtIndexOf(i)+ Check.AtIndexOf(i);
+            // System.out.println(Integer.sum(Check, Check));
+            // int Check = (String) Check 
+            // Check = Check + Check;
+
+            
+            System.out.println(Check);
+
+
+
+            // if(UserInput == Check ){
+            // System.out.println(UserInput + "is an armstrong number");
+            // }
+
+            // else(UserInput !== Check){
+            //     System.out.println(UserInput + "is not an armstrong number");
+            // }
+           
+            
+        }
+    }
+}
+
+        // Character.getNumericValue(number.charAt(i));
+
+        // int number = (int) UserInput.charAt(i);
+        // this cast it from string to integers I get unicode back. must turn unicode back ro numbers
+        
+        //  this gives me the number from unicode
+
+        // int sum = Math.pow(a,Digits) 
+        // MAth.pow( once I get numbers seprated, digits)
+        // this is how you raise a number to another number
+        
+        // System.out.println(numbers);
+
+        // double a=30;
+        // double b=2;
+        // System.out.println(Math.pow(a,b));
+        // System.out.println(Math.pow(a,b));
+
+
+        //  153 ==  (1^3 + 5^3 + 5^3)
+        //
+        // step one take user input
+        // - scanner
+        //  step 2 check if its armostrong number
+        //  -(153)
+        // - (see how many digits are in each number .length())
+        // for loop (for int i=0; i<UserInput.length; i++)
+        // 
+        // - (store that number of digits 
+        //   int container = userinput.length)
+        // 
+        // - (seperate each number 
+        //  userInput.substring() 
+        //    userInput.charAt(i))
+        //  charAt() 
+        // casting to integer it gets unicode()
+        //   Character.getNumericValue(number.charAt(i))
+        // )
+        // - exponent each seperate number to the amount of digits
+        //  -
+        // 153 
+        // Math.pow(a,b)
+        // Math.pow(index 0,Userinput.length) = a
+        // Math.pow(index 1,Userinput.length) = b
+        //  sum = a + b
+        // if(userInput == sum) =true  
+        // if(userInput == sum) = false
+
+        // - compare that sum
+        // to the initial value inputted
+        // true = its armotrong
+        // false its not armstrong 
+        // n that is the sum
+        // of its own digits 
+        // each raised to the power
+        // of the number of digits
+        
+        
+
+
+
+
+
+    
